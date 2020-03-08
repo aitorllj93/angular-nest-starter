62dcfa17c5a1b0dae507ba4edbf97cbbac8f0c3d
aitorllj93 <aitorllj93@gmail.com>
2020-03-08 15:36:31 +0100

    add ng-select

--- apps/app/src/app/ui/ui.module.ts
+++ apps/app/src/app/ui/ui.module.ts
@@ -3,15 +3,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   imports: [
     CommonModule,
-    NgxDatatableModule
+    NgxDatatableModule,
+    NgSelectModule
   ],
   exports: [
     CommonModule,
-    NgxDatatableModule
+    NgxDatatableModule,
+    NgSelectModule
   ],
   providers: [],
 })
---
+++ apps/app/src/stories/2-Select.stories.ts
@@ -0,0 +1,49 @@
+
+import { moduleMetadata } from '@storybook/angular';
+import { object, text } from '@storybook/addon-knobs';
+
+import { NgSelectModule } from '@ng-select/ng-select';
+
+export default {
+  title: 'Select',
+  decorators: [
+    moduleMetadata({
+      imports: [NgSelectModule],
+    }),
+  ],
+};
+
+export const SimpleSelect = () => ({
+  title: 'Select',
+  template: `
+    <ng-select [items]="items"
+      bindLabel="name"
+      bindValue="id"
+      [(ngModel)]="selectedPersonId">
+    </ng-select>
+  `,
+  props: {
+    items: object('Items', [
+      {
+          'id': '5a15b13c36e7a7f00cf0d7cb',
+          'name': 'Karyn Wright',
+      },
+      {
+          'id': '5a15b13c2340978ec3d2c0ea',
+          'name': 'Rochelle Estes',
+      },
+      {
+          'id': '5a15b13c663ea0af9ad0dae8',
+          'name': 'Mendoza Ruiz',
+      }
+    ]),
+    selectedPersonId: text('Selected Person Id', '5a15b13c663ea0af9ad0dae8'),
+  }
+});
+
+SimpleSelect.story = {
+  name: 'Simple Select',
+  parameters: {
+    notes: 'Simple Select'
+  },
+};
