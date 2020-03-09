69e4a14bbb77ba57f67afb9de053d99f63909eea
Aitor Llamas Jiménez <allamas@bilbomatica.es>
2020-03-09 08:57:23 +0100

    add some margin to button stories

--- apps/app/src/stories/02-Button.stories.ts
+++ apps/app/src/stories/02-Button.stories.ts
@@ -19,25 +19,27 @@ export default {
 
 export const ButtonStyles = () => ({
   template: `
-    <button mdc-button>Default</button>
+    <div class="storybook-wrapper">
+      <button style="margin: 10px;" mdc-button>Default</button>
 
-    <a mdc-button href="#/button-demo/examples">Href link</a>
+      <a style="margin: 10px;" mdc-button href="#/button-demo/examples">Href link</a>
 
-    <a mdc-button raised href="#/button-demo/examples">Href link</a>
+      <a style="margin: 10px;" mdc-button raised href="#/button-demo/examples">Href link</a>
 
-    <button mdc-button raised>Raised</button>
+      <button style="margin: 10px;" mdc-button raised>Raised</button>
 
-    <button mdc-button unelevated>Unelevated</button>
+      <button style="margin: 10px;" mdc-button unelevated>Unelevated</button>
 
-    <button mdc-button outlined>Outlined</button>
+      <button style="margin: 10px;" mdc-button outlined>Outlined</button>
 
-    <button mdc-button outlined class="demo-thick-outline-button">Thick Outlined</button>
+      <button style="margin: 10px;" mdc-button outlined class="demo-thick-outline-button">Thick Outlined</button>
 
-    <button mdc-button disabled>Disabled</button>
+      <button style="margin: 10px;" mdc-button disabled>Disabled</button>
 
-    <button mdc-button class="demo-button-primary">Primary Color</button>
+      <button style="margin: 10px;" mdc-button class="demo-button-primary">Primary Color</button>
 
-    <button mdc-button class="demo-button-secondary">Secondary Color</button>
+      <button style="margin: 10px;" mdc-button class="demo-button-secondary">Secondary Color</button>
+    </div>
   `
 });
 
@@ -47,11 +49,13 @@ ButtonStyles.story = {
 
 export const ButtonDensity = () => ({
   template: `
-    <button mdc-button class="demo-density-button">Density (-3)</button>
+    <div class="storybook-wrapper">
+      <button style="margin: 10px;" mdc-button class="demo-density-button">Density (-3)</button>
 
-    <button mdc-button raised class="demo-density-button">Density (-3)</button>
+      <button style="margin: 10px;" mdc-button raised class="demo-density-button">Density (-3)</button>
 
-    <button mdc-button outlined class="demo-density-button">Density (-3)</button>
+      <button style="margin: 10px;" mdc-button outlined class="demo-density-button">Density (-3)</button>
+    </div>
   `
 });
 
@@ -61,48 +65,50 @@ ButtonDensity.story = {
 
 export const ButtonIcons = () => ({
   template: `
-    <button mdc-button>
-      <mdc-icon>favorite</mdc-icon>Icon
-    </button>
+    <div class="storybook-wrapper">
+      <button style="margin: 10px;" mdc-button>
+        <mdc-icon>favorite</mdc-icon>Icon
+      </button>
 
-    <button mdc-button outlined>
-      <mdc-icon>favorite</mdc-icon>Icon
-    </button>
+      <button style="margin: 10px;" mdc-button outlined>
+        <mdc-icon>favorite</mdc-icon>Icon
+      </button>
 
-    <button mdc-button raised class="demo-button-primary">
-      <mdc-icon>favorite</mdc-icon>Icon
-    </button>
+      <button style="margin: 10px;" mdc-button raised class="demo-button-primary">
+        <mdc-icon>favorite</mdc-icon>Icon
+      </button>
 
-    <button mdc-button raised class="demo-button-secondary">
-      <mdc-icon>favorite</mdc-icon>Icon
-    </button>
+      <button style="margin: 10px;" mdc-button raised class="demo-button-secondary">
+        <mdc-icon>favorite</mdc-icon>Icon
+      </button>
 
-    <button mdc-button raised>
-      <mdc-icon>
-        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#000000">
-          <path fill="none" d="M0 0h24v24H0z" />
-          <path d="M23 12c0-6.07-4.93-11-11-11S1 5.93 1 12s4.93 11 11 11 11-4.93 11-11zM5 17.64C3.75 16.1 3
-          14.14 3 12c0-2.13.76-4.08 2-5.63v11.27zM17.64 5H6.36C7.9 3.75 9.86 3 12 3s4.1.75 5.64 2zM12 14.53L8.24
-          7h7.53L12 14.53zM17 9v8h-4l4-8zm-6 8H7V9l4 8zm6.64 2c-1.55 1.25-3.51 2-5.64 2s-4.1-.75-5.64-2h11.28zM21
-          12c0 2.14-.75 4.1-2 5.64V6.37c1.24 1.55 2 3.5 2 5.63z" />
-        </svg>
-      </mdc-icon>
-      SVG Icon
-    </button>
+      <button style="margin: 10px;" mdc-button raised>
+        <mdc-icon>
+          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#000000">
+            <path fill="none" d="M0 0h24v24H0z" />
+            <path d="M23 12c0-6.07-4.93-11-11-11S1 5.93 1 12s4.93 11 11 11 11-4.93 11-11zM5 17.64C3.75 16.1 3
+            14.14 3 12c0-2.13.76-4.08 2-5.63v11.27zM17.64 5H6.36C7.9 3.75 9.86 3 12 3s4.1.75 5.64 2zM12 14.53L8.24
+            7h7.53L12 14.53zM17 9v8h-4l4-8zm-6 8H7V9l4 8zm6.64 2c-1.55 1.25-3.51 2-5.64 2s-4.1-.75-5.64-2h11.28zM21
+            12c0 2.14-.75 4.1-2 5.64V6.37c1.24 1.55 2 3.5 2 5.63z" />
+          </svg>
+        </mdc-icon>
+        SVG Icon
+      </button>
 
-    <button mdc-button>
-      <span mdcButtonLabel>Trailing Icon</span>
-      <mdc-icon>favorite</mdc-icon>
-    </button>
+      <button style="margin: 10px;" mdc-button>
+        <span mdcButtonLabel>Trailing Icon</span>
+        <mdc-icon>favorite</mdc-icon>
+      </button>
 
-    <button mdc-button raised class="demo-button-primary">
-      <mdc-button-label>Trailing Icon</mdc-button-label>
-      <mdc-icon>favorite</mdc-icon>
-    </button>
+      <button style="margin: 10px;" mdc-button raised class="demo-button-primary">
+        <mdc-button-label>Trailing Icon</mdc-button-label>
+        <mdc-icon>favorite</mdc-icon>
+      </button>
 
-    <button mdc-button outlined label="Trailing Icon">
-      <mdc-icon>favorite</mdc-icon>
-    </button>
+      <button style="margin: 10px;" mdc-button outlined label="Trailing Icon">
+        <mdc-icon>favorite</mdc-icon>
+      </button>
+    </div>
   `
 });
 
@@ -112,13 +118,15 @@ ButtonIcons.story = {
 
 export const ButtonThemes = () => ({
   template: `
-    <button mdc-button unelevated class="demo-button-ink-color">
-      <mdc-icon>favorite</mdc-icon>Ink Color
-    </button>
+    <div class="storybook-wrapper">
+      <button style="margin: 10px;" mdc-button unelevated class="demo-button-ink-color">
+        <mdc-icon>favorite</mdc-icon>Ink Color
+      </button>
 
-    <button mdc-button outlined class="demo-button-icon-color">
-      <mdc-icon>favorite</mdc-icon>Icon Color
-    </button>
+      <button style="margin: 10px;" mdc-button outlined class="demo-button-icon-color">
+        <mdc-icon>favorite</mdc-icon>Icon Color
+      </button>
+    </div>
   `
 });
 
@@ -128,11 +136,13 @@ ButtonThemes.story = {
 
 export const ButtonShaped = () => ({
   template: `
-    <button mdc-button unelevated class="big-round-shape-button">Corner Radius</button>
+    <div class="storybook-wrapper">
+      <button style="margin: 10px;" mdc-button unelevated class="big-round-shape-button">Corner Radius</button>
 
-    <button mdc-button raised class="big-round-shape-button">
-      <mdc-icon>favorite</mdc-icon>Icon
-    </button>
+      <button style="margin: 10px;" mdc-button raised class="big-round-shape-button">
+        <mdc-icon>favorite</mdc-icon>Icon
+      </button>
+    </div>
   `
 });
 
@@ -142,8 +152,10 @@ ButtonShaped.story = {
 
 export const ButtonAccesibility = () => ({
   template: `
-    <div class="mdc-touch-target-wrapper">
-      <button mdc-button touch>My Accessible Button</button>
+    <div class="storybook-wrapper">
+      <div class="mdc-touch-target-wrapper">
+        <button mdc-button touch>My Accessible Button</button>
+      </div>
     </div>
   `
 });
