d4fffcecf3f34be9421c7268b2fc8c3180865fb4
d3v0ps <aitorllj93@gmail.com>
2020-07-18 17:29:20 +0200

    add icons to attributes card

--- apps/app/src/app/people/components/person-attributes-card.component.ts
+++ apps/app/src/app/people/components/person-attributes-card.component.ts
@@ -11,16 +11,19 @@ import { RandomPersonGeneratorService } from '../services/random-person-generato
           <div [fxFlex]="50" class="px-1">
 
             <p class="mt-0 person-attributes-text-strength">
+              <mdc-icon fontSet="mdi" fontIcon="mdi-arm-flex"></mdc-icon>
               Strength: {{ person.attributes.strength }}
               <mdc-slider class="person-attributes-slider-strength" discrete [min]="0" [max]="20" [step]="1" [value]="person.attributes.strength" [disabled]="true"></mdc-slider>
             </p>
 
             <p class="mt-0 person-attributes-text-constitution">
+              <mdc-icon fontSet="mdi" fontIcon="mdi-shield"></mdc-icon>
               Constitution: {{ person.attributes.constitution }}
               <mdc-slider class="person-attributes-slider-constitution" discrete [min]="0" [max]="20" [step]="1" [value]="person.attributes.constitution" [disabled]="true"></mdc-slider>
             </p>
 
             <p class="mt-0 person-attributes-text-dexterity">
+              <mdc-icon fontSet="mdi" fontIcon="mdi-run-fast"></mdc-icon>
               Dexterity: {{ person.attributes.dexterity }}
               <mdc-slider class="person-attributes-slider-dexterity" discrete [min]="0" [max]="20" [step]="1" [value]="person.attributes.dexterity" [disabled]="true"></mdc-slider>
             </p>
@@ -30,16 +33,19 @@ import { RandomPersonGeneratorService } from '../services/random-person-generato
           <div [fxFlex]="50" class="px-1">
 
             <p class="mt-0 person-attributes-text-intelligence">
+              <mdc-icon fontSet="mdi" fontIcon="mdi-head-lightbulb"></mdc-icon>
               Intelligence: {{ person.attributes.intelligence }}
               <mdc-slider class="person-attributes-slider-intelligence" discrete [min]="0" [max]="20" [step]="1" [value]="person.attributes.intelligence" [disabled]="true"></mdc-slider>
             </p>
 
             <p class="mt-0 person-attributes-text-wisdom">
+              <mdc-icon fontSet="mdi" fontIcon="mdi-book"></mdc-icon>
               Wisdom: {{ person.attributes.wisdom }}
               <mdc-slider class="person-attributes-slider-wisdom" discrete [min]="0" [max]="20" [step]="1" [value]="person.attributes.wisdom" [disabled]="true"></mdc-slider>
             </p>
 
             <p class="mt-0 person-attributes-text-charisma">
+              <mdc-icon fontSet="mdi" fontIcon="mdi-account-voice"></mdc-icon>
               Charisma: {{ person.attributes.charisma }}
               <mdc-slider class="person-attributes-slider-charisma" discrete [min]="0" [max]="20" [step]="1" [value]="person.attributes.charisma" [disabled]="true"></mdc-slider>
             </p>
